81aa44ce43e3285cee2d04179e441e5573ff8c8c
18348375641 <a11083879A>
2023-11-02 09:36:54 +0800

    设计模式 -- 策略模式

---
+++ Java-Work-Demo/src/main/java/设计模式/策略模式/代码/Client.java
@@ -0,0 +1,27 @@
+package 设计模式.策略模式.代码;
+
+/**
+ * @author zyz
+ * @version 1.0
+ * @data 2023/11/2 9:35
+ * @Description:
+ */
+class Client {
+    public static void main(String args[]) {
+        MovieTicket mt = new MovieTicket();
+        double originalPrice = 60.0;
+        double currentPrice;
+
+        mt.setPrice(originalPrice);
+        System.out.println("原始价为：" + originalPrice);
+        System.out.println("---------------------------------");
+
+        Discount discount;
+        discount = new StudentDiscount();
+        mt.setDiscount(discount); //注入折扣对象
+
+        currentPrice = mt.getPrice();
+        System.out.println("折后价为：" + currentPrice);
+    }
+}
+
