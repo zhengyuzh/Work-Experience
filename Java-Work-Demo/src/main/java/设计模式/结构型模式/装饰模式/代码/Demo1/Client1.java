912d0bd2950407e6fe34ae6463b2cba96ee5782c
18348375641 <a11083879A>
2023-11-02 13:26:12 +0800

    设计模式--结构型模式 -- 装饰模式

---
+++ Java-Work-Demo/src/main/java/设计模式/结构型模式/装饰模式/代码/Demo1/Client1.java
@@ -0,0 +1,18 @@
+package 设计模式.结构型模式.装饰模式.代码.Demo1;
+
+/**
+ * @author zyz
+ * @version 1.0
+ * @data 2023/11/2 11:17
+ * @Description:
+ */
+public class Client1 {
+    public static void main(String[] args) {
+        Component component,component1,component2;
+        component = new Window();
+        component1 = new ScrollBarDecorator(component);
+
+        component2 = new BlackBorderDecorator(component1);
+        component2.display();
+    }
+}
