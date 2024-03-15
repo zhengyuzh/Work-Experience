1a449778cc498866a8dacabcbfe0ae3fcd43aab0
18348375641 <a11083879A>
2024-03-15 11:15:58 +0800

    【51】、字符串拼接 + 号形式

---
+++ Java-Work-Demo/src/main/java/A4字符串转换/B6字符串拼接/Demo/StringConcatenation.java
@@ -0,0 +1,26 @@
+package A4字符串转换.B6字符串拼接.Demo;
+
+import org.junit.Test;
+
+/**
+ * @BelongsProject: Java-Work-Demo
+ * @BelongsPackage: A4字符串转换.B6字符串拼接.Demo
+ * @Author: zhengyuzhu
+ * @CreateTime: 2024-02-20  11:30
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class StringConcatenation {
+
+    /**
+     * @description: 1、字符串拼接 字符串和图片格式拼接
+     * @author: zhengyuzhu
+     * @date: 2024/2/20 11:31
+     **/
+    @Test
+    public void test(){
+        String faceProtocol = "ATM_FACE_AGR";
+        String fileName = faceProtocol +".jpg";
+        System.out.println(fileName);
+    }
+}
